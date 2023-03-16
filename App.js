975d20cfe2968a481ae1b712d463785753644df0
coderchrisdean <coderchrisdean@gmail.com>
2023-03-16 16:37:00 -0700

    added App.js to use new component

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GithubRepoGenerator from './GithubRepoGenerator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GithubRepoGenerator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
